3ac6e2a81c105de63e885063d9b91f1a05f5b5fc
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2020-12-15 13:15:27 +0000

    (Bug/Major) Cached data for EC2 status were never flushed.

--- src/interact.py
+++ src/interact.py
@@ -34,9 +34,7 @@ class QueryCache:
         if date != self.last_call_date:
             self.cache          = {}
             self.last_call_date = date
-            self.load_cached    = True
-        else:
-            self.load_cached    = False
+            self.interact_precomputed_data = None
 
     def get(self, url, variant=None, default=None):
         key = "%s_%s" % (url, variant)
@@ -242,6 +240,7 @@ class Interact:
             return False
 
         if "instanceid" in event: instance_id = event["instanceid"] # Override from query string
+        query_cache.load_cached_data()
         d        = next(filter(lambda d: d["InstanceId"] == instance_id, cacheddata), None)
         if d is None:
             response["statusCode"] = 400
