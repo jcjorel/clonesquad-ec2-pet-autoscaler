c20fb3e5e57f8f32f5198f9dea4880669aaab374
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2021-03-24 15:43:06 +0000

    (Bug/Major) Fixed improprer management of notification events when, for any reason, a large amount is pending for processing.

--- src/notify.py
+++ src/notify.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import boto3
 import json
@@ -385,30 +386,54 @@ improve CloneSquad over time by allowing easy sharing of essential data for remo
         }
         events_r = events.copy()
         events_r.reverse()
+
         # Optimize message size by deduplicating metadata
-        if len(events_r):
-            m = events_r[0]["Metadata"]
-            for i in range(1, len(events_r)):
+        messages = []
+        while len(events_r):
+            # Sanity check
+            msg["Events"] = [events_r[0]]
+            msg_size      = len(json.dumps(msg, default=str))
+            if msg_size > 256*1024:
+                event_type = events_r[0]["EventType"]
+                event_date = events_r[0]["EventDate"]
+                log.error(f"Notification message too big ({msg_size} > 256kB)! Possible cause is too many instances "
+                    "under management... This message {event_date}/{event_type} will be discarded...")
+                events_r = events_r[1:]
+                continue
+
+            chunk = []
+            m     = None
+            index = 0
+            for i in range(0, len(events_r)):
+                index = i
+                # Size control
+                msg["Events"] = chunk.copy()
+                if i < len(events_r)-1:
+                    msg["Events"].append(events_r[i+1])
+                if len(json.dumps(msg, default=str)) > 256*1024:
+                    # Chunk is going to be too big. Create a new chunk now...
+                    break
+                if "Metadata" not in events_r[i]:
+                    log.error("Malformed event %s/%s read from DynamoDB! (???) Skipping it..." % 
+                            (events_r[i]["EventDate"], events_r[i]["EventType"]))
+                    continue
                 meta = events_r[i]["Metadata"]
                 if m == meta:
                     del events_r[i]["Metadata"]
                 else:
                     m = meta
+                chunk.append(events_r[i])
+            messages.append(chunk)
+            events_r = events_r[index+1:]
 
-        while len(notification_message.keys()) and len(events_r):
-            events_to_send    = len(events_r)
-            msg["Events"]     = events_r[:events_to_send]
+        for m in messages:
+            if len(m) == 0:
+                continue
+            msg["Events"]     = m
             content_str       = json.dumps(msg, default=str)
-            # Verify that message is not too big to send
-            while len(content_str) >= 256*1024 and events_to_send > 2:
-                events_to_send -= 1
-                while ("Metadata" not in events_r[events_to_send]):
-                    events_to_send -= 1
-                msg["Events"] = events_r[:events_to_send]
-                content_str   = json.dumps(msg, default=str)
 
             event_types = []
-            [event_types.append(e["EventType"]) for e in events_r[:events_to_send] if e["EventType"] not in event_types]
+            [event_types.append(e["EventType"]) for e in m if e["EventType"] not in event_types]
             
             log.log(log.NOTICE, "Notification payload size: %s" % len(content_str))
             for arn in notification_message.keys():
@@ -427,15 +452,14 @@ improve CloneSquad over time by allowing easy sharing of essential data for remo
                 except Exception as e:
                     log.warning("Failed to notify '%s'! Got Exception: %s" % (arn, e))
 
-            events_r = events_r[events_to_send:]
-            if len(events_r):
+            #if len(events_r):
                 # To help ordering of messages. Wait wait a few seconds...
-                seconds_to_wait = Cfg.get_int("notify.event.seconds_between_sending")
-                log.log(log.NOTICE, "Had to split the notification messages as too big to fit in 256kB. "
-                        f"Wait {seconds_to_wait} seconds before to send the remaining events. "
-                        "Note: This can happen when no user notification target is acknowledging the event and so they stack for a while. "
-                        "Please consider acknowledging all events as soon as generated.")
-                time.sleep(seconds_to_wait)
+            #    seconds_to_wait = Cfg.get_int("notify.event.seconds_between_sending")
+            #    log.log(log.NOTICE, "Had to split the notification messages as too big to fit in 256kB. "
+            #            f"Wait {seconds_to_wait} seconds before to send the remaining events. "
+            #            "Note: This can happen when no user notification target is acknowledging the event and so they stack for a while. "
+            #            "Please consider acknowledging all events as soon as generated.")
+            #    time.sleep(seconds_to_wait)
 
     @xray_recorder.capture()
     def call_lambda(self, arn, region, account_id, service_path, content, e):
