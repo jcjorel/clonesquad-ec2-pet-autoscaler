eef6cc36f395aa32ab7d202f303afd451dff5eef
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2021-04-21 10:32:08 +0000

    .

---
+++ tools/generate-metadata-athena-ddl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+S3_URL=$1
+
+if [ -z "$S3_URL" ] ; then
+	echo "Usage: $0 <S3 location>" ; exit 1
+fi
+
+tmpdir=$(mktemp -d)
+mkdir -p $tmpdir/metadata
+aws s3 sync $S3_URL $tmpdir/metadata
+launchdir=$(pwd)
+cd $tmpdir/metadata
+for table in * ; do
+	${launchdir}/tools/quick-and-dirty-aws-athena-ddl-generator-for-json `find $table -name '*.json' -print` \
+		--table-name "clonesquad_${table}" --location ${S3_URL}/${table}
+done
