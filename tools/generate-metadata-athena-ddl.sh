616781746998486df9290261ca977ceca7203117
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2021-04-21 10:35:22 +0000

    .

--- tools/generate-metadata-athena-ddl.sh
+++ tools/generate-metadata-athena-ddl.sh
@@ -13,5 +13,6 @@ launchdir=$(pwd)
 cd $tmpdir/metadata
 for table in * ; do
 	${launchdir}/tools/quick-and-dirty-aws-athena-ddl-generator-for-json `find $table -name '*.json' -print` \
-		--table-name "clonesquad_${table}" --location ${S3_URL}/${table}
+		--table-name "clonesquad_${table}" --location ${S3_URL}/${table} \
+		--partitioned-by "PARTITIONED BY (accountid string, region string, groupname string)"
 done
