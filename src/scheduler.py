01942bf42d75aff5244aa60ffb0799028fa9f3cb
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2021-04-26 16:54:55 +0000

    .

--- src/scheduler.py
+++ src/scheduler.py
@@ -43,7 +43,7 @@ class Scheduler:
                 "Format": "String",
                 "Description": """Cron job specification for [Backup and Metadata](BACKUP_AND_METADATA.md) generation.
 
-This setting control when Configuration/Scheduler DynamoDB tables are backuped. It also defines when the Metadata files, queriable with AWS Athena, are generated.
+This setting control when Configuration/Scheduler DynamoDB tables are backuped. It also defines when the Metadata files, queriable with AWS Athena, are generated. The format follows the `cron(...)` and `localcron(...)` keywords as defined in the [scheduler documentation](SCHEDULER.md).
 
 By default, an hourly cron job is defined.
 
