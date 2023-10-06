f73607454d21de108039b990d73df398c43cfcf4
Jean-Charles JOREL <jeancharlesjorel@gmail.com>
2023-10-06 11:00:15 +0000

    (Major) Upgrade Lambda Python runtime to 3.11 (Note: only marginal performance improvments expected as CloneSquad is spending lots of time calling AWS APIs). Upgraded Python dependencies to latest versions.

--- SOURCE-ME!!.sh
+++ SOURCE-ME!!.sh
@@ -1,4 +1,4 @@
 export CLONESQUAD_DIR=$PWD/
-export CLONESQUAD_DEPENDENCY_DIR=$CLONESQUAD_DIR/.venv/lib/python3.7/site-packages/
+export CLONESQUAD_DEPENDENCY_DIR=$CLONESQUAD_DIR/.venv/lib/python3.11/site-packages/
 export CLONESQUAD_PARAMETER_DIR=~/clonesquad-deployment-parameters/
 export CLONESQUAD_DEVKIT_IMAGE=${CLONESQUAD_DEVKIT_IMAGE:-"clonesquad/devkit"}
